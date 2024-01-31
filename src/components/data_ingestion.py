c394cba9cf021f5e33d7081969876cd0e822ea96
sathvikbalu <sathvik.balu@gmail.com>
2024-01-31 16:27:56 +0530

    Data transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,10 @@ from src.logger import logging
 import pandas as pd 
 
 from sklearn.model_selection import train_test_split
-
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 
 #to provide input(ie where to store train, test, data) to data ingestion component, we provide it through DataIngestionConfig class.
@@ -52,4 +54,7 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
